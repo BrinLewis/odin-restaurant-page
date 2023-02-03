53857150eb5c75d1f91f7d1e1b5151ca10a25e38
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-03 11:56:15 +0000

    Re-create navbar using javascript

--- dist/main.js
+++ dist/main.js
@@ -7,26 +7,90 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("console.log(\"Hello Brinsley!\");\n\n//# sourceURL=webpack://odin-restaurant-page/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _render_navbar__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./render-navbar */ \"./src/render-navbar.js\");\n\n\n(0,_render_navbar__WEBPACK_IMPORTED_MODULE_0__.renderNav)();\n\n// import home-page.js and call loadHomePage function\n// import menu-page.js and call loadMenuPage function\n// import about-page.js and call loadAboutPage function\n\n//# sourceURL=webpack://odin-restaurant-page/./src/index.js?");
+
+/***/ }),
+
+/***/ "./src/render-navbar.js":
+/*!******************************!*\
+  !*** ./src/render-navbar.js ***!
+  \******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"renderNav\": () => (/* binding */ renderNav)\n/* harmony export */ });\nfunction renderNav () {\n  const header = document.querySelector(\".header\");\n\n  // Create logo\n  const logo = document.createElement(\"p\");\n  logo.classList.add(\"logo\");\n  logo.textContent = \"Odin's\";\n  header.appendChild(logo);\n\n  // Create nav-bar\n  const nav = document.createElement(\"nav\");\n  header.appendChild(nav);\n  const navItems = document.createElement(\"ul\");\n  nav.appendChild(navItems);\n\n  // Create items for nav-bar\n  const aboutBtn = document.createElement(\"li\");\n  aboutBtn.textContent = \"About\";\n  navItems.appendChild(aboutBtn);\n  const homeBtn = document.createElement(\"li\");\n  homeBtn.textContent = \"Home\";\n  navItems.appendChild(homeBtn);\n  const menuBtn = document.createElement(\"li\");\n  menuBtn.textContent = \"Menu\";\n  navItems.appendChild(menuBtn);\n};\n\n\n\n//# sourceURL=webpack://odin-restaurant-page/./src/render-navbar.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
