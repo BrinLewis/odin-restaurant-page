6b9ce1f0d01697b6a6e9b753de3276d0ce23dc33
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-03 16:32:20 +0000

    Import styles, images and foundations/home modules Attach imported images to their nodes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-import { renderNav } from "./render-navbar";
+import "./styles.css"
+import restaurantImg from "./assets/restaurant.jpg";
+import { renderFoundations } from "./render-foundations";
+import { loadHomePage } from "./home-page";
+import pizzaLogo from "./assets/pizza-logo.svg";
 
-renderNav();
+renderFoundations();
+loadHomePage();
+backgroundImg.src = restaurantImg;
+pizzaImg.src = pizzaLogo;
 
 // import home-page.js and call loadHomePage function
 // import menu-page.js and call loadMenuPage function
