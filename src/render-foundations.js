536325556a7b348bab9ab809c48775c49d5da5bb
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-03 17:37:55 +0000

    Add image credits footer

--- src/render-foundations.js
+++ src/render-foundations.js
@@ -30,6 +30,27 @@ function renderFoundations() {
   const menuBtn = document.createElement("li");
   menuBtn.textContent = "Menu";
   navItems.appendChild(menuBtn);
+
+  // Create background image credits footer
+  const photoCredit = document.createElement("p");
+  photoCredit.classList.add("image-credits")
+
+  const profileLink = document.createElement("a");
+  profileLink.textContent = "Jason Leung"
+  profileLink.setAttribute("href", "https://unsplash.com/@ninjason");
+  const siteLink = document.createElement("a");
+  siteLink.textContent = "Unsplash"
+  siteLink.setAttribute("href", "https://unsplash.com/photos/poI7DelFiVA");
+
+  const ptext1 = document.createTextNode("Photo by ");
+  const ptext2 = document.createTextNode(" on ");
+
+  photoCredit.appendChild(ptext1);
+  photoCredit.appendChild(profileLink);
+  photoCredit.appendChild(ptext2);
+  photoCredit.appendChild(siteLink);
+
+  document.body.appendChild(photoCredit);
 }
 
 export { renderFoundations };
