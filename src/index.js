88d9151318adeed44a0215330a42c42a17f65d9d
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-06 14:59:54 +0000

    Create function to highlight selected nav btn

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { loadHomePage } from "./home-page";
 
 renderFoundations();
 loadHomePage();
+highlightNavBtn("home");
 
 function clearContent() {
   const content = document.querySelector(".content");
@@ -13,23 +14,35 @@ function clearContent() {
   }
 };
 
-// const aboutBtn = document.querySelector(".about-btn");
-// aboutBtn.addEventListener("click", () => {
-//   clearContent();
-//   loadAboutPage();
-// });
+function highlightNavBtn (currentPage) {
+  const navItems = document.querySelectorAll(".nav-btn");
+  navItems.forEach(el => {
+    el.classList.remove("current-page");
+  });
+  const currentNavItem = document.getElementById(`${currentPage}-btn`);
+  currentNavItem.classList.add("current-page");
+};
 
-const homeBtn = document.querySelector(".home-btn");
+const aboutBtn = document.getElementById("about-btn");
+aboutBtn.addEventListener("click", () => {
+  clearContent();
+  // loadAboutPage();
+  highlightNavBtn("about");
+});
+
+const homeBtn = document.getElementById("home-btn");
 homeBtn.addEventListener("click", () => {
   clearContent();
   loadHomePage();
+  highlightNavBtn("home");
 });
 
-// const menuBtn = document.querySelector(".menu-btn");
-// menuBtn.addEventListener("click", () => {
-//   clearContent();
-//   loadMenuPage();
-// });
+const menuBtn = document.getElementById("menu-btn");
+menuBtn.addEventListener("click", () => {
+  clearContent();
+  // loadMenuPage();
+  highlightNavBtn("menu");
+});
 
 
 // import home-page.js and call loadHomePage function
