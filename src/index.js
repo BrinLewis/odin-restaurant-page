7bfc4cfe5b32f680cff985ed2414f0357af2bb00
BrinLewis <brinsley22@hotmail.co.uk>
2023-02-06 13:11:14 +0000

    Remove image imports (no-longer need to import) Create clearContent function and nav btn event listeners

--- src/index.js
+++ src/index.js
@@ -1,13 +1,36 @@
 import "./styles.css"
-import restaurantImg from "./assets/restaurant.jpg";
 import { renderFoundations } from "./render-foundations";
 import { loadHomePage } from "./home-page";
-import pizzaLogo from "./assets/pizza-logo.svg";
 
 renderFoundations();
 loadHomePage();
-backgroundImg.src = restaurantImg;
-pizzaImg.src = pizzaLogo;
+
+function clearContent() {
+  const content = document.querySelector(".content");
+
+  while(content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+};
+
+// const aboutBtn = document.querySelector(".about-btn");
+// aboutBtn.addEventListener("click", () => {
+//   clearContent();
+//   loadAboutPage();
+// });
+
+const homeBtn = document.querySelector(".home-btn");
+homeBtn.addEventListener("click", () => {
+  clearContent();
+  loadHomePage();
+});
+
+// const menuBtn = document.querySelector(".menu-btn");
+// menuBtn.addEventListener("click", () => {
+//   clearContent();
+//   loadMenuPage();
+// });
+
 
 // import home-page.js and call loadHomePage function
 // import menu-page.js and call loadMenuPage function
